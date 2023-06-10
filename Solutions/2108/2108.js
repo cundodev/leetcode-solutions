492d82a9d4093b903e0a140e8e6ce3c1e34a64ab
facindito <facinditto@gmail.com>
2023-06-09 21:07:03 -0300

    #2108

--- Solutions/2108/2108.js
+++ Solutions/2108/2108.js
@@ -1,3 +1,5 @@
+//2108. Find First Palindromic String in the Array
+
 export const firstPalindrome = (words) => {
   const palindromic = words.find(
     (word) => word === [...word].reverse().join("")
