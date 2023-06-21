4b738194cf8a25d9c55aff91f047df2de00542f2
facindito <facinditto@gmail.com>
2023-06-21 01:17:11 -0300

    347

---
+++ Solutions/347.js
@@ -0,0 +1,13 @@
+//347. Top K Frequent Elements
+
+const topKFrequent = (nums, k) => {
+  const frequents = {}
+  nums.forEach( num =>{
+    frequents[num] = frequents[num] ? frequents[num] + 1 : 1
+  })
+  return Object
+    .entries(frequents)
+    .sort(([$,a],[$$,b])=> b - a)
+    .map(([key,_]) => Number(key))
+    .slice(0,k)
+}
