f33b555e0a0a31ba559085e2af01b1d60e64a541
cundodev <facinditto@gmail.com>
2024-06-19 23:30:06 -0300

    add 14 solution

---
+++ Solutions/14.ts
@@ -0,0 +1,16 @@
+// 14. Longest Common Prefix
+function longestCommonPrefix(strs: string[]): string {
+  let prefix = strs[0]
+  if (!strs) return ''
+  if (strs.length === 1) return strs[0]
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, prefix.length - 1)
+    }
+  }
+  return prefix
+}
+
+longestCommonPrefix(['flower', 'flow', 'flight']) //"fl"
+longestCommonPrefix(['dog', 'racecar', 'car']) // ""
+longestCommonPrefix(['']) //""
