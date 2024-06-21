85c18edc108169817f5a72171b7862d2311c1d6b
cundodev <facinditto@gmail.com>
2024-06-20 23:25:55 -0300

    add 14 solution

--- Solutions/14.ts
+++ Solutions/14.ts
@@ -11,6 +11,18 @@ function longestCommonPrefix(strs: string[]): string {
   return prefix
 }
 
+function longestCommonPrefix2(strs: string[]): string {
+  let prefix = strs[0]
+  if (!strs) return ''
+  if (strs.length === 1) return strs[0]
+  for (let i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, prefix.length - 1)
+    }
+  }
+  return prefix
+}
+
 longestCommonPrefix(['flower', 'flow', 'flight']) //"fl"
 longestCommonPrefix(['dog', 'racecar', 'car']) // ""
 longestCommonPrefix(['']) //""
